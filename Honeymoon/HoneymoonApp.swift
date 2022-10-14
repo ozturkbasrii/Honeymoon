54fdc1d229a9ce8a096a78d83a308ac49f957ee7
Hasan Basri Öztürk <ozturkbasrii@gmail.com>
2022-10-14 12:56:07 +0300

    Initial Commit

---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Hasan Basri Öztürk on 14.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
